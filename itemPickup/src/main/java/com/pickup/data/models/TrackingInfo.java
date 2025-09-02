72e810c2bb78e9387566a0b5656800fd43e1cf75
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-02 20:31:40 +0100

    entity for tracking info fields

---
+++ itemPickup/src/main/java/com/pickup/data/models/TrackingInfo.java
@@ -0,0 +1,11 @@
+package com.pickup.data.models;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class TrackingInfo {
+    private LocalDateTime createdAt;
+    private String event;
+}
