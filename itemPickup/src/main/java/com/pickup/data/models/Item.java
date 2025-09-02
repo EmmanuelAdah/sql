79d13f0def6b823022c39a2d83bfb144993349ef
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-02 20:34:51 +0100

    Entity for item's fields

---
+++ itemPickup/src/main/java/com/pickup/data/models/Item.java
@@ -0,0 +1,21 @@
+package com.pickup.data.models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@Document
+public class Item {
+    @Id
+    private String itemId;
+    private String itemName;
+    private String weightInGrams;
+    private String description;
+
+    @DBRef
+    private TrackingInfos trackingInfos;
+}
