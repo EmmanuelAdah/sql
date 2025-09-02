f4832a0bebe2b9a8523fa3ad201930eea46f0031
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-02 20:37:02 +0100

    Items test file for possible edge cases

---
+++ itemPickup/src/test/java/com/pickup/data/repository/ItemsTest.java
@@ -0,0 +1,22 @@
+package com.pickup.data.repository;
+
+import com.pickup.data.models.Item;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataMongoTest
+class ItemsTest {
+
+    @Autowired
+    private Items items;
+
+    @Test
+    void countTest() {
+        items.deleteAll();
+        items.save(new Item());
+        assertEquals(1L, items.count());
+    }
+}
