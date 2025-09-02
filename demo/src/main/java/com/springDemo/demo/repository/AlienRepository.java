ccc43f97f9b4938de430d78e066b82be1b874631
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-02 20:30:36 +0100

    To perform logics need for manipulation of the database

---
+++ demo/src/main/java/com/springDemo/demo/repository/AlienRepository.java
@@ -0,0 +1,34 @@
+package com.springDemo.demo.repository;
+
+import com.springDemo.demo.models.Alien;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public class AlienRepository {
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void jdbcConfig(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void saveAlien(Alien alien) {
+        String sql = "INSERT INTO alien (id, name, tech) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sql, alien.getId(), alien.getName(), alien.getTech());
+    }
+
+    public List<Alien> findAll() {
+        String sql = "SELECT * FROM alien";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            Alien alien = new Alien();
+            alien.setId(rs.getInt("id"));
+            alien.setName(rs.getString("name"));
+            alien.setTech(rs.getString("tech"));
+            return alien;
+        });
+    }
+}
+
