ea35fa544f96451c1a5355ebe2a88b63495073eb
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-03 21:07:59 +0100

    item service implementation

---
+++ itemPickup/src/main/java/com/pickup/services/ItemServiceImpl.java
@@ -0,0 +1,21 @@
+package com.pickup.services;
+
+import com.pickup.data.models.Item;
+import com.pickup.data.repository.Items;
+import com.pickup.dtos.request.SendItemRequest;
+import com.pickup.dtos.response.SendItemResponse;
+import org.springframework.stereotype.Service;
+
+import static com.pickup.utils.Mapper.map;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+    private Items items;
+
+    SendItemResponse sendItem(SendItemRequest sendItemRequest){
+        Item item = map(sendItemRequest);
+        items.save(item);
+        SendItemResponse sendItemResponse = map(item);
+        return sendItemResponse;
+    }
+}
