9d3a174df7836ed8724970768ffcfd38d6c2017f
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-05 19:36:38 +0100

    pickup application items repository test

--- itemPickup/src/test/java/com/pickup/data/repository/ItemsTest.java
+++ itemPickup/src/test/java/com/pickup/data/repository/ItemsTest.java
@@ -4,7 +4,6 @@ import com.pickup.data.models.Item;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataMongoTest
