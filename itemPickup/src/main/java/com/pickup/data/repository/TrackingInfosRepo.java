e426ba139b811bd55386b49a561b5ea261dcc87d
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-05 19:38:32 +0100

    For accessing tracking infos database

--- itemPickup/src/main/java/com/pickup/data/repository/TrackingInfosRepo.java
+++ itemPickup/src/main/java/com/pickup/data/repository/TrackingInfosRepo.java
@@ -1,6 +1,5 @@
 package com.pickup.data.repository;
 
-import com.pickup.data.models.Item;
 import com.pickup.data.models.TrackingInfos;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
