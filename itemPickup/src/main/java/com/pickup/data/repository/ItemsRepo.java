d3e24698ccd6f157899bb59b07f6b9aa86cc7dfd
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-05 23:50:11 +0100

    Item repository

--- itemPickup/src/main/java/com/pickup/data/repository/ItemsRepo.java
+++ itemPickup/src/main/java/com/pickup/data/repository/ItemsRepo.java
@@ -5,6 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Items extends MongoRepository<Item, String> {
+public interface ItemsRepo extends MongoRepository<Item, String> {
 
 }
