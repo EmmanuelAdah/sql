0d5e6e8c80fa40c001238f0cc1173127bf0fba5e
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-05 23:46:55 +0100

    sql schema file for alien table

--- demo/src/main/resources/schema.sql
+++ demo/src/main/resources/schema.sql
@@ -1,5 +1,5 @@
-CREATE TABLE alien(
-    id INT PRIMARY KEY,
-    name VARCHAR(25),
-    tech VARCHAR(25)
-);
+# CREATE TABLE alien(
+#     id INT PRIMARY KEY,
+#     name VARCHAR(25),
+#     tech VARCHAR(25)
+# );
