da869e3e038ddaa44bb96b786410ec677fbe805d
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-06 22:01:01 +0100

    alien controller for user request and response

--- demo/src/main/java/com/springDemo/demo/controller/AlienController.java
+++ demo/src/main/java/com/springDemo/demo/controller/AlienController.java
@@ -1,14 +1,25 @@
 package com.springDemo.demo.controller;
 
+import com.springDemo.demo.dtos.requests.AlienRequest;
+import com.springDemo.demo.dtos.response.AlienResponse;
 import com.springDemo.demo.models.Alien;
-import com.springDemo.demo.repository.AlienRepository;
-import org.springframework.stereotype.Component;
+import com.springDemo.demo.services.AlienService;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
-@Component
+@RestController
 public class AlienController {
-     private AlienRepository repo;
+     private AlienService alienService;
 
-    public void addAlien(Alien alien){
-        repo.saveAlien(alien);
+    public AlienResponse addAlien(AlienRequest alienRequest){
+        return alienService.saveAlien(alienRequest);
+    }
+
+    public List<Alien> findAllAlien() {
+        return alienService.findAll();
+    }
+
+    public String deleteAll(){
+        return alienService.deleteAll();
     }
 }
