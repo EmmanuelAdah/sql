08585646998707cdd1ec406232c63bc89b0b19a3
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-07 01:41:01 +0100

    For data validations and mapping

--- demo/src/main/java/com/springDemo/demo/services/AlienService.java
+++ demo/src/main/java/com/springDemo/demo/services/AlienService.java
@@ -4,15 +4,22 @@ import com.springDemo.demo.dtos.requests.AlienRequest;
 import com.springDemo.demo.dtos.response.AlienResponse;
 import com.springDemo.demo.models.Alien;
 import com.springDemo.demo.repositories.AlienRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 import static com.springDemo.demo.utils.Mapper.map;
 
 @Service
 public class AlienService {
-    private AlienRepository alienRepository;
+    private final AlienRepository alienRepository;
 
-    public AlienResponse saveAlien(AlienRequest alienRequest) {
+    @Autowired
+    public AlienService(AlienRepository alienRepository) {
+        this.alienRepository = alienRepository;
+    }
+
+    public AlienResponse saveAlien(@RequestBody AlienRequest alienRequest) {
         Alien alien = map(alienRequest);
         alienRepository.saveAlien(alien);
         return map(alien);
