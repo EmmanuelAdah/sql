b5f021721b4ec8c0840717b73a9cd4babd2a06c6
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-07 01:39:07 +0100

    For user request and server response to the user interface

--- demo/src/main/java/com/springDemo/demo/controller/AlienController.java
+++ demo/src/main/java/com/springDemo/demo/controller/AlienController.java
@@ -4,21 +4,31 @@ import com.springDemo.demo.dtos.requests.AlienRequest;
 import com.springDemo.demo.dtos.response.AlienResponse;
 import com.springDemo.demo.models.Alien;
 import com.springDemo.demo.services.AlienService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 public class AlienController {
-     private AlienService alienService;
+     private final AlienService alienService;
 
-    public AlienResponse addAlien(AlienRequest alienRequest){
+    @Autowired
+    public AlienController(AlienService alienService) {
+        this.alienService = alienService;
+    }
+
+    @PostMapping
+    public AlienResponse addAlien(@RequestBody AlienRequest alienRequest){
         return alienService.saveAlien(alienRequest);
     }
 
+    @GetMapping
     public List<Alien> findAllAlien() {
         return alienService.findAll();
     }
 
+    @DeleteMapping
     public String deleteAll(){
         return alienService.deleteAll();
     }
