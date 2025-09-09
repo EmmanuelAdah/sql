9db0a95f35148b6995f7ec91c21753530419ee8a
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-09 10:55:33 +0100

    Alien controller for request from the user interface and response from the server API

--- demo/src/main/java/com/springDemo/demo/controller/AlienController.java
+++ demo/src/main/java/com/springDemo/demo/controller/AlienController.java
@@ -6,7 +6,6 @@ import com.springDemo.demo.models.Alien;
 import com.springDemo.demo.services.AlienService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -18,17 +17,27 @@ public class AlienController {
         this.alienService = alienService;
     }
 
-    @PostMapping
+    @PostMapping("/alien")
     public AlienResponse addAlien(@RequestBody AlienRequest alienRequest){
         return alienService.saveAlien(alienRequest);
     }
 
-    @GetMapping
+    @GetMapping("/get/{id}")
+    public AlienResponse findAlienById(@PathVariable int id){
+        return alienService.findByAlienId(id);
+    }
+
+    @GetMapping("/get/{name}")
+    public AlienResponse findAlienByName(@PathVariable String name){
+        return alienService.findByAlienName(name);
+    }
+
+    @GetMapping("/aliens")
     public List<Alien> findAllAlien() {
         return alienService.findAll();
     }
 
-    @DeleteMapping
+    @DeleteMapping("/delete/aliens")
     public String deleteAll(){
         return alienService.deleteAll();
     }
