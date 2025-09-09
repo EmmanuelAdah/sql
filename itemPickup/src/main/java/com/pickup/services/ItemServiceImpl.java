e89045deb848ee3032a86593db0a3e8b8c452573
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-09 10:59:15 +0100

    Implementation of the item service for validation and mapping of fields

--- itemPickup/src/main/java/com/pickup/services/ItemServiceImpl.java
+++ itemPickup/src/main/java/com/pickup/services/ItemServiceImpl.java
@@ -5,16 +5,25 @@ import com.pickup.data.repository.ItemsRepo;
 import com.pickup.dtos.request.SendItemRequest;
 import com.pickup.dtos.response.SendItemResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
 import static com.pickup.utils.Mapper.map;
 
 @Service
 public class ItemServiceImpl implements ItemService {
     private ItemsRepo itemsRepo;
 
-    SendItemResponse sendItem(SendItemRequest sendItemRequest){
+    public SendItemResponse sendItem(SendItemRequest sendItemRequest){
         Item item = map(sendItemRequest);
         itemsRepo.save(item);
         SendItemResponse sendItemResponse = map(item);
         return sendItemResponse;
+    }
+
+    public Optional<Item> findAll(){
+        List<Item> items = itemsRepo.findAll();
+        return Optional.of(items);
     }
 }
