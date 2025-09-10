8a91b9dec0180d294382a3ccc9935ca22b4b7735
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-10 14:44:56 +0100

    Response field for the user interface

--- demo/src/main/java/com/springDemo/demo/dtos/response/AlienResponse.java
+++ demo/src/main/java/com/springDemo/demo/dtos/response/AlienResponse.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Component;
 public class AlienResponse {
     private int id;
     private String name;
-    private String tech;
+    private String techStack;
 }
