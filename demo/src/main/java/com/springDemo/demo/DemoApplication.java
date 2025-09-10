1162fdd2aa619e24b66d406ccad9b553af269b12
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-10 14:46:10 +0100

    Spring boot bean

--- demo/src/main/java/com/springDemo/demo/DemoApplication.java
+++ demo/src/main/java/com/springDemo/demo/DemoApplication.java
@@ -1,10 +1,7 @@
 package com.springDemo.demo;
 
-import com.springDemo.demo.controller.AlienController;
-import com.springDemo.demo.dtos.requests.AlienRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,27 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 
 	public static void main(String... args) {
-		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-
-		AlienController controller = context.getBean(AlienController.class);
-		System.out.println(controller.deleteAll());
-
-		AlienRequest alienRequest = context.getBean(AlienRequest.class);
-		alienRequest.setId(1);
-		alienRequest.setName("Emma");
-		alienRequest.setTech("Java, Python");
-
-		controller.addAlien(alienRequest);
-
-		AlienController controller1 = context.getBean(AlienController.class);
-		AlienRequest alienRequest1 = new AlienRequest();
-		alienRequest1.setId(2);
-		alienRequest1.setName("James");
-		alienRequest1.setTech("Java, JavaScript, Python");
-		controller1.addAlien(alienRequest1);
-
-		System.out.println(controller.findAllAlien());
-		System.out.println(controller.findAlienById(1));
-		System.out.println(controller.findAlienByName("James"));
+		SpringApplication.run(DemoApplication.class, args);
 	}
 }
