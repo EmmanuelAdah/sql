045dea7b9aa74f9701caeb3c5be0ff70917d135b
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-10 14:45:54 +0100

    Request entity for fields from the user interface

--- demo/src/main/java/com/springDemo/demo/dtos/requests/AlienRequest.java
+++ demo/src/main/java/com/springDemo/demo/dtos/requests/AlienRequest.java
@@ -1,14 +1,12 @@
 package com.springDemo.demo.dtos.requests;
 
-import jakarta.persistence.Id;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
 public class AlienRequest {
-    @Id
     private int id;
     private String name;
-    private String tech;
+    private String techStack;
 }
